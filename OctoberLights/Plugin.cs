e024c64625aa5ffa85bc800b8f33258f40ebb01e
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2021-10-15 23:01:36 +0200

    Add files via upload

---
+++ OctoberLights/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using Exiled.API.Features;
+using System;
+using ServerHandler = Exiled.Events.Handlers.Server;
+
+namespace OctoberLights
+{
+    public class Plugin : Plugin<Cfg>
+    {
+        public static Plugin Instance;
+        public override string Author { get; } = "xNexus";
+        public override string Name { get; } = "October Lights";
+        public override string Prefix { get; } = "October Lights";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(3, 0, 5);
+        public OctoberLightsEvents events;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            events = new OctoberLightsEvents();
+            Log.Info("OctoberLights Loaded, Enjoy Halloween");
+
+            ServerHandler.RoundStarted += events.OnRoundStarted;
+
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Instance = null;
+            events = null;
+
+            ServerHandler.RoundStarted -= events.OnRoundStarted;
+
+            base.OnDisabled();
+        }
+    }
+}
