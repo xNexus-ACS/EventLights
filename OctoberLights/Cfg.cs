0f18f4e8027d220311cb7e6a55c8ba6a6872431b
xNexus <83370388+xNexus-ACS@users.noreply.github.com>
2021-10-15 22:59:43 +0200

    Add files via upload

---
+++ OctoberLights/Cfg.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace OctoberLights
+{
+    public class Cfg : IConfig
+    {
+        [Description("Is the plugin Enabled?")]
+        public bool IsEnabled { get; set; } = true;
+    }
+}
